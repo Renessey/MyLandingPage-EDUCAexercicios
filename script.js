ca8d0515df9d597d8243338a6dde27193d9fe690
Renessey <144975138+Renessey@users.noreply.github.com>
2024-06-07 19:39:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+const dynamicButton = document.getElementById('dynamicButton');
+
+dynamicButton.addEventListener('click', () => {
+    alert("Copiado.")
+});
+
+const dynamic = document.getElementById('dynamic');
+
+dynamic.addEventListener('click', () => {
+    alert('Compartilhado.');
+});
+
